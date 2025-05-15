a257d916a9b2320cfcf006b50a2765dba0f4a781
Pablo <pablitolestau@hotmail.com>
2025-05-15 23:48:35 +0200

    Ejercicio 2

--- Assets/Scripts/CodePanel.cs
+++ Assets/Scripts/CodePanel.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public class CodePanel : MonoBehaviour
 {
-    public TextMeshProUGUI codeDisplay;
-    public string correctCode = "4271";
+    public TextMeshProUGUI codeDisplay, resultado;
+    public string correctCode;
     private string currentInput = "";
 
     public void PressButton(string number)
@@ -14,21 +14,44 @@ public class CodePanel : MonoBehaviour
 
         currentInput += number;
         codeDisplay.text = currentInput;
+    }
 
+    public void PressDelete()
+    {
+        if (currentInput.Length > 0)
+        {
+            currentInput = currentInput.Substring(0, currentInput.Length - 1);
+            codeDisplay.text = currentInput;
+        }
+    }
+
+    public void PressEnter()
+    {
         if (currentInput.Length == correctCode.Length)
         {
             if (currentInput == correctCode)
-                Debug.Log("✅ Código correcto");
+            {
+                codeDisplay.text = "";
+                resultado.text = "Codigo correcto";
+            }
             else
-                Debug.Log("❌ Código incorrecto");
-
-            Invoke(nameof(ResetCode), 2f);
+            {
+                codeDisplay.text = "";
+                resultado.text = "Codigo incorrecto";
+            }
         }
+        else
+        {
+            codeDisplay.text = "";
+            resultado.text = "Codigo incompleto";
+        }
+        Invoke(nameof(ResetCode), 2f);
     }
 
     private void ResetCode()
     {
         currentInput = "";
         codeDisplay.text = "";
+        resultado.text = "";
     }
 }
